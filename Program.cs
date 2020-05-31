0a07d7ca0fd286a1ff24f65f22e23abab5ad160c
MichalLeszczynski <mleszczynsk@gmail.com>
2020-05-31 09:14:19 +0200

    Refactor sql files

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 
 namespace CompanyApi
 {
+
     class Program
     {
         static void Main(string[] args)
---
+++ delete_procedures.sql
@@ -0,0 +1,35 @@
+use CompanyDB
+go  
+
+/*Delete*/
+drop table Employees
+go
+
+drop procedure AddEmployee
+go
+
+drop procedure RemoveEmployee
+go
+
+drop procedure RemoveAllEmployees
+go
+
+drop procedure GetEmployee
+go
+
+drop procedure GetEmployeeWithChildren
+go
+
+drop procedure GetAllEmployees
+go
+
+drop procedure GetMaxSalary
+go
+
+drop procedure GetAverageSalary
+go
+
+drop procedure GetSumSalary
+go
+
+    
